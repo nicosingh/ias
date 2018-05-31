b910a46105ffe9cf6bfc34cdd946c2a5030fb31a
Filippo-Fagioli <33449219+FaFilippo@users.noreply.github.com>
2018-05-31 10:35:23 +0200

    Remove sh file
    Remove sh file  and substitute with py file

---
+++ Utils/test/runTests.py
@@ -0,0 +1,12 @@
+import IASTools
+import os
+from IASLogging.logConf import Log
+
+if __name__=="__main__":
+ os.system("pwd")
+ log=Log()
+ logger=log.initLogging(os.path.basename(__file__),"info","info")
+ logger.info("Start testModel")
+ os.system("testCreateModule")
+ os.system("iasRun -l s -v true org.scalatest.run org.eso.ias.utils.test.ISO8601Test")
+
